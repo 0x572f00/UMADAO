1ed87a5082adbf6871c3d4e7c900265ba5c09ace
matascup <57924511+matascup@users.noreply.github.com>
2023-08-24 03:47:38 +0400

    Fixed Mint Link
    Redirected the Mint button to lead to a working page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -253,7 +253,7 @@ try {
         </section>
 
         <section id="mint">
-            <a href="https://ipfs.io/ipfs/bafybeiev2gmkthauyzpvagqcs77lhoo4aoe5cgb6xk76uy3gwfrfnrh644/nft-drop.html?contract=0x2381B67c6f1CB732fDf8b3B29d3260eC6F7420bC&chainId=1" target="_blank" rel="nofollow">
+            <a href="https://embed.ipfscdn.io/ipfs/bafybeihazpt6pkm4azgtupdz7hc2j3o4zpjsvtcgfq4t2keozxkss3ud6q/?contract=0x2381B67c6f1CB732fDf8b3B29d3260eC6F7420bC&chain=%7B%22name%22%3A%22Ethereum+Mainnet%22%2C%22chain%22%3A%22ETH%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fethereum.rpc.thirdweb.com%2F%24%7BTHIRDWEB_API_KEY%7D%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Ether%22%2C%22symbol%22%3A%22ETH%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22eth%22%2C%22chainId%22%3A1%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22ethereum%22%2C%22icon%22%3A%7B%22url%22%3A%22ipfs%3A%2F%2FQmcxZHpyJa8T4i63xqjPYrZ6tKrt55tZJpbXcjSDKuKaf9%2Fethereum%2F512.png%22%2C%22height%22%3A512%2C%22width%22%3A512%2C%22format%22%3A%22png%22%7D%7D&clientId=4151c615f1904ce872ce08ca2fe34c49&theme=dark&primaryColor=blue" target="_blank" rel="nofollow">
               <img src="./img/bubble.png" alt=""/>
               <span>Mint</span>
             </a>
